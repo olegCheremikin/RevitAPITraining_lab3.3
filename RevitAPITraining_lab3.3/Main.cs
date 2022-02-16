60efd3be5179e0a1dd509776c2bc8b88f46d740e
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2022-02-16 13:22:29 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_lab3.3/Main.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_lab3._3
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedPipesRefList = uidoc.Selection.PickObjects(ObjectType.Element, new PipeFilter(), "Выберите трубы");
+
+            foreach (var selectedPipeRef in selectedPipesRefList)
+            {
+                Pipe pipe = doc.GetElement(selectedPipeRef) as Pipe;
+                Parameter lengthParameter = pipe.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH);
+                if (lengthParameter.StorageType == StorageType.Double)
+                {
+                    using (Transaction ts = new Transaction(doc, "Set parameters"))
+                    {
+                        ts.Start();
+                        Parameter lengthPlusParameter = pipe.LookupParameter("Длина с запасом");
+                        double lenghtPlus = lengthParameter.AsDouble() / 1000 * 1.1;                        
+                        lengthPlusParameter.Set(lenghtPlus);
+                        ts.Commit();
+                    }
+                }
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPITraining_lab3.3/PipeFilter.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_lab3._3
+{
+    class PipeFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Pipe;
+        }
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
